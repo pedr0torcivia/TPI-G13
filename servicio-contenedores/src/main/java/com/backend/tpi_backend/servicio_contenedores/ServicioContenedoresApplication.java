0cb7383556173a58a099b1c7201a137db86ce01a
Pedro Torcivia <torcivia.p3dro@gmail.com>
2025-11-05 09:29:06 -0300

    Primer commit Servicio Contenedores

---
+++ servicio-contenedores/src/main/java/com/backend/tpi_backend/servicio_contenedores/ServicioContenedoresApplication.java
@@ -0,0 +1,13 @@
+package com.backend.tpi_backend.servicio_contenedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioContenedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioContenedoresApplication.class, args);
+	}
+
+}
---
+++ servicio-contenedores/src/test/java/com/backend/tpi_backend/servicio_contenedores/ServicioContenedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.tpi_backend.servicio_contenedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioContenedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
